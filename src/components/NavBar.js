67784f1a073395ad0311ab789aa93f90a0e2a901
ONJoseph <on.josephinfo@gmail.com>
2023-01-24 11:29:30 +0100

    Add isActive functionality to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <span className="brand">Bookstore CMS</span>
-      {' '}
-      <ul>
-        <li><Link className="nav-link" to="/">BOOKS</Link></li>
-        <li><Link className="nav-link" to="/categories">CATEGORIES</Link></li>
-      </ul>
-    </nav>
+    <div className="nav-bar">
+      <div className="inner-nav-bar">
+        <NavLink className="nav-link" to="/">
+          <h2>Bookstore CMS</h2>
+        </NavLink>
+        <ul>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link inactive')} to="/">
+              Books
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link inactive')} to="/categories">
+              Categories
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
