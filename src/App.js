f20de6f4efd554b4c4e0b46f2c44ed6b5c29efae
ONJoseph <on.josephinfo@gmail.com>
2023-01-25 01:33:11 +0100

    connect store to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Books from './Pages/Books';
-import Categories from './Pages/Categories';
-import NavBar from './components/NavBar';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <Books />
+    </Provider>
   );
 }
+
+export default App;
