f4ecad78096788cd77d779db967f288507e07f5d
ONJoseph <on.josephinfo@gmail.com>
2023-01-25 01:25:03 +0100

    onnect your app to the Redux store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,27 +3,35 @@ const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 // Initial State
-const initialState = [];
+const initialState = [
+  { id: 1, title: 'Book 1', author: 'Author 1' },
+  { id: 2, title: 'Book 2', author: 'Author 2' },
+  { id: 3, title: 'Book 3', author: 'Author 3' },
+];
 
 // Action Creators
-export const addBook = (book) => ({
+const addBook = (book) => ({
   type: ADD_BOOK,
-  book,
+  payload: book,
 });
 
-export const removeBook = (book) => ({
+const removeBook = (book) => ({
   type: REMOVE_BOOK,
-  book,
+  payload: book,
 });
+
+export { addBook, removeBook };
 
 // Reducer
-export default (state = initialState, action) => {
+const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.book];
+      return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((b) => b.id !== action.book.id);
+      return state.filter((b) => b.id !== action.payload.id);
     default:
       return state;
   }
 };
+
+export default booksReducer;
