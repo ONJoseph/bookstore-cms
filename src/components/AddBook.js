9cf5334ced1ee2cfb7b94933601bbfd666bf3b1b
ONJoseph <on.josephinfo@gmail.com>
2023-01-25 01:23:50 +0100

    change default state in books reducer

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from './booksSlice';
 
-export default function AddBook() {
+function AddNewBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: Date.now(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+  };
+
   return (
     <section>
       <h2>ADD NEW BOOK</h2>
-      <form>
-        <input type="text" placeholder="Book title" />
-        <input type="text" placeholder="Author" />
-        <input type="submit" value="ADD BOOK" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Book author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button type="submit">Add</button>
       </form>
     </section>
   );
 }
+
+export default AddNewBook;
