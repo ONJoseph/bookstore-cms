e234e1850821f3925dbb0fcd1c609f393d85a8b6
ONJoseph <on.josephinfo@gmail.com>
2023-01-27 16:09:12 +0100

    Used createAsyncThunk for thunk action creators

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createAsyncThunk, thunkMiddleware } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
+
+export const fetchBooks = createAsyncThunk('books/fetch', async () => {
+  const response = await fetch('/api/books');
+  const data = await response.json();
+  return data;
+});
+
+export const fetchCategories = createAsyncThunk('categories/fetch', async () => {
+  const response = await fetch('/api/categories');
+  const data = await response.json();
+  return data;
+});
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     categories: categoriesReducer,
   },
+  middleware: [thunkMiddleware],
 });
 
 export default store;
