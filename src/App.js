0eed3ef73b415a7990bd5d56bd60dff869d9f5ed
ONJoseph <on.josephinfo@gmail.com>
2023-01-28 16:29:27 +0100

    Import App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Navbar from './components/Nav/Navbar';
 import Categories from './components/categories/Categories';
 import Home from './pages/home/Home';
@@ -7,19 +7,22 @@ import Store from './pages/store/Store';
 import BookDetails from './components/bookDetails/BookDetails';
 import Contacts from './pages/contacts/Contacts';
 
-const App = () => (
-  <>
-    <div className="app-container">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/store/:id" element={<BookDetails />} />
-      </Routes>
-    </div>
-  </>
-);
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/categories" component={Categories} />
+          <Route path="/contacts" component={Contacts} />
+          <Route path="/store" exact component={Store}>
+            <Route path="/store/:id" component={BookDetails} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
