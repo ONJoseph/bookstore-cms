b5fb46b76d3715d9d652ad144938ddce95f193e8
ONJoseph <on.josephinfo@gmail.com>
2023-01-29 03:18:40 +0100

    fixed linter errors

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,25 +1,6 @@
 const STATUS_CHECKED = 'STATUS_CHECKED';
 
-const initialState = [
-  {
-    id: '1',
-    author: 'Joseph',
-    title: 'How to Achieve Mastery in Software Development',
-    category: 'Software',
-  },
-  {
-    id: '2',
-    author: 'John Smith',
-    title: 'The Great Gatsby',
-    category: 'Fiction',
-  },
-  {
-    id: '3',
-    author: 'Akasha',
-    title: 'The Unbearable Lightness of Being',
-    category: 'Lifestyle',
-  },
-];
+const initialState = [];
 
 const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
